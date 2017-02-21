1fa57664c283eec226ba57bfbf4ad89d31192154
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2017-02-21 13:06:46 +0000

    Project structure

---
+++ AgileQuizSoftware/src/java/AgileQuiz/stores/LoggedIn.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AgileQuiz.stores;
+
+/**
+ *
+ * @author gary-
+ */
+public class LoggedIn {
+    boolean loggedIn = false;
+    
+    public void LoggedIn(){
+        
+    }
+    
+    public void setLoggedIn(){
+        loggedIn=true;
+    }
+    public void setLoggedOut(){
+        loggedIn=false;
+    }
+    
+}
